1630e606ff0a9c72c1940f1a9d89ac01d07b37b1
Jeongwoo Park <wahoo9040@naver.com>
2024-12-23 16:32:50 +0900

    chore: Hydration 에러 임시 삭제 추후 변경

--- components/photoList/PhotoList.tsx
+++ components/photoList/PhotoList.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 
 import styles from "./PhotoList.module.css";
@@ -5,37 +6,10 @@ import { QueryErrorResetBoundary, useSuspenseQueries, useSuspenseQuery } from "@
 import { getMediaDetail, getMediaIds } from "@/src/api/instagram";
 import { Suspense } from "react";
 
-import Bat from "../loading/bat/Bat";
+import Bat from "../loadingBat/Bat";
 import { MdErrorOutline } from "react-icons/md";
 import { ErrorBoundary } from "react-error-boundary";
 import QueryProvider from "../_providers/QueryProvider";
-
-function Photos() {
-  const { data } = useSuspenseQuery({ queryFn: getMediaIds, queryKey: ["instagram-photos"] });
-  const instagramIds = data.map(({ id }) => id);
-
-  const { photos } = useSuspenseQueries({
-    queries: instagramIds.slice(0, 20).map((instagramId) => ({
-      queryKey: ["instagram-photo", instagramId],
-      queryFn: () => getMediaDetail({ instagramId }),
-      staleTime: Infinity,
-    })),
-    combine: (results) => ({
-      photos: results.flatMap((result) => result.data).filter((v) => !!v),
-      isPending: results.some((result) => result.isPending),
-    }),
-  });
-
-  return (
-    <>
-      {photos.slice(0, 15).map(({ id, media_url }) => (
-        <div key={id} className={styles["photo_wrapper"]}>
-          <img src={media_url} className={styles.photo} alt="instagram photo" />
-        </div>
-      ))}
-    </>
-  );
-}
 
 export default function PhotoList() {
   return (
@@ -72,5 +46,33 @@ export default function PhotoList() {
         </QueryErrorResetBoundary>
       </QueryProvider>
     </div>
+  );
+}
+
+function Photos() {
+  const { data } = useSuspenseQuery({ queryFn: getMediaIds, queryKey: ["instagram-photos"] });
+  const instagramIds = data.map(({ id }) => id);
+
+  const { photos } = useSuspenseQueries({
+    queries: instagramIds.slice(0, 20).map((instagramId) => ({
+      queryKey: ["instagram-photo", instagramId],
+      queryFn: () => getMediaDetail({ instagramId }),
+      staleTime: Infinity,
+    })),
+    combine: (results) => ({
+      photos: results.flatMap((result) => result.data).filter((v) => !!v),
+      isPending: results.some((result) => result.isPending),
+    }),
+  });
+
+  // TODO: initital Data 사용으로 suppressHydrationWarning 삭제
+  return (
+    <>
+      {photos.slice(0, 15).map(({ id, media_url }) => (
+        <div key={id} className={styles["photo_wrapper"]}>
+          <img src={media_url} className={styles.photo} alt="instagram photo" suppressHydrationWarning />
+        </div>
+      ))}
+    </>
   );
 }
